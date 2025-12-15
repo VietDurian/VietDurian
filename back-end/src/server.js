45c5fe2f2fced552723bd8924a1ab4674b17e9d0
Thuy Vi <vivltce170398@fpt.edu.vn>
2025-12-16 01:22:56 +0700

    ViVLT: FIX CONFLIC

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./config/swagger");
-const API_v1 = require("./routes/index");
-const connectDB = require("./config/mongoose");
+import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "@/config/swagger";
+import { API_v1 } from "@/routes/index";
+import connectDB from "@/config/mongoose";
 require("dotenv").config();
 
 const app = express();
