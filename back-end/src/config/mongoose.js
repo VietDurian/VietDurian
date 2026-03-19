561bd8cb2bd368d10ce6e4fb71fc7872e3937f72
ChanNG <145467988+ChanPotter@users.noreply.github.com>
2026-03-19 15:07:00 +0700

    fix

--- back-end/src/config/mongoose.js
+++ back-end/src/config/mongoose.js
@@ -5,18 +5,18 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    // const uri = process.env.MONGODB_URI;
-    const uri = "mongodb+srv://ChanPotter:Password123!@cluster0.jdsmxen.mongodb.net/?appName=Cluster0";
+    const uri = process.env.MONGODB_URI;
+    // const uri = "mongodb+srv://ChanPotter:Password123!@cluster0.jdsmxen.mongodb.net/?appName=Cluster0";
     const dbName = process.env.DATABASE_NAME;
 
     if (!uri) {
       throw new Error("MONGODB_URI_ATLAS is not defined in .env file");
     }
 
-    // await mongoose.connect(uri, {
-    //   dbName: dbName,
-    // });
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      dbName: dbName,
+    });
+    // await mongoose.connect(uri);
 
     console.log("MongoDB Connected Successfully");
   } catch (error) {
