efbb210fe9a5116076689db4672ac4364d903e4d
Nguyen Trong Quy <trongquy131204@gmail.com>
2026-03-19 15:49:15 +0700

    deploy

--- front-end/web/next.config.mjs
+++ front-end/web/next.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: [
-			'res.cloudinary.com',
-			'lh3.googleusercontent.com',
-			'picsum.photos',
-			'encrypted-tbn0.gstatic.com',
-			'gstatic.com',
-			'imgur.com',
-			'unsplash.com',
-			'pexels.com',
-			'i.imgur.com',
-			'images.unsplash.com',
-			'images.pexels.com',
-		],
-		unoptimized: true, // Allow all external images without optimization
-	},
-	typescript: {
-		// Allow JS-only projects to build even if TS errors exist
-		ignoreBuildErrors: true,
-	},
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "picsum.photos",
+      "encrypted-tbn0.gstatic.com",
+      "gstatic.com",
+      "imgur.com",
+      "unsplash.com",
+      "pexels.com",
+      "i.imgur.com",
+      "images.unsplash.com",
+      "images.pexels.com",
+    ],
+    unoptimized: true, // Allow all external images without optimization
+  },
+  typescript: {
+    // Allow JS-only projects to build even if TS errors exist
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
